50f19d7ccd8074f84f5085af0b1effda6ccfb0ec
tsutomu katsube <tsuto.igawa@gmail.com>
2013-10-04 14:24:07 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tsuwari::Application.config.secret_key_base = 'f10c74b34874bdb9b670237564561a3b75429a42825e78820b5d485b25a476b6452ac0f090f564e020a257c347f91ce88f1ff7b5336b16233244a6b0b3d3286d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tsuwari::Application.config.session_store :cookie_store, key: '_tsuwari_session'
