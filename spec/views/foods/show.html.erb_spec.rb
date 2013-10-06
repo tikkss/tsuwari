9af2387a677992c4300361ae0b9db1e7522e2195
sousk333 <fire.bluestar@gmail.com>
2013-10-06 11:28:43 +0900

    Modified. food/show 味表示追加

--- spec/views/foods/show.html.erb_spec.rb
+++ spec/views/foods/show.html.erb_spec.rb
@@ -2,14 +2,20 @@ require 'spec_helper'
 
 describe "foods/show" do
   before(:each) do
+    @expect_food  = "カレー"
+    @expect_taste = %w(甘い 辛い 酸っぱい)
     @food = assign(:food, stub_model(Food,
-      :name => "Name"
+      name: @expect_food,
+      food_tastes: @expect_taste.map do |taste_name|
+        stub_model(FoodTaste, taste: stub_model(Taste, name: taste_name))
+      end
     ))
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
+    expect(rendered).to match(/#{@expect_food}/)
+    expect(rendered).to match(/#{@expect_taste.join(", ")}/)
   end
 end
