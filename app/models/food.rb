3317038ac75eb1eb775ba3f7dc0a087beb85f3b1
sousk333 <fire.bluestar@gmail.com>
2013-10-06 19:22:01 +0900

    Added. Food#have_taste? 特定の味を持っているかの確認メソッド

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,14 @@
 class Food < ActiveRecord::Base
-  has_many :food_tastes
+  has_many :food_tastes, dependent: :destroy
   has_many :tastes, through: :food_tastes
 
-  accepts_nested_attributes_for :food_tastes
+  accepts_nested_attributes_for :food_tastes, reject_if: :all_blank
+
+  # この料理が指定された味を持っているかどうか
+  #
+  # @param [Fixnum] taste_id 味ID
+  # @return [TrueClass, FalseClass] 持っているとtrue
+  def have_taste?(taste_id)
+    self.taste_ids.include?(taste_id)
+  end
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe Food do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#have_taste?" do
+    before do
+      @food = Food.create!(name: :curry)
+      @taste_a = Taste.create!(name: :aaaaaaaaaaaaaaaaaaaaa)
+      @taste_b = Taste.create!(name: :bbbbbbbbbbbbbbbbbbbbb)
+    end
+
+    context "味を持っている場合" do
+      before do
+        @food.update(food_tastes_attributes: [{taste_id: @taste_a.id}])
+      end
+
+      subject { @food.have_taste?(@taste_a.id) }
+      it "trueが返ること" do
+        expect(subject).to be_true
+      end
+    end
+
+    context "味を持っていない場合" do
+      before do
+        @food.update(food_tastes_attributes: [{taste_id: @taste_a.id}])
+      end
+
+      subject { @food.have_taste?(@taste_b.id) }
+      it "falseが返ること" do
+        expect(subject).to be_false
+      end
+    end
+
+    context "そもそも味情報を一つも持っていない場合" do
+      subject { @food.have_taste?(@taste_a.id) }
+      it "falseが返ること" do
+        expect(subject).to be_false
+      end
+    end
+  end
 end
