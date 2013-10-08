ac1843acb235c73a109b28b869b59661e8dd6afb
sousk333 <fire.bluestar@gmail.com>
2013-10-08 21:29:17 +0900

    Added. Food name validate

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,6 @@
 class Food < ActiveRecord::Base
   has_many :food_tastes, dependent: :destroy
   has_many :tastes, through: :food_tastes
+
+  validates :name, presence: true, length: { maximum: 255 }
 end
