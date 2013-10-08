10aa618fadc5c7c1b0aea2fc29f8031c1dd7e8de
sousk333 <fire.bluestar@gmail.com>
2013-10-08 20:26:35 +0900

    Modified. Food#have_taste? の削除

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,12 +1,4 @@
 class Food < ActiveRecord::Base
   has_many :food_tastes, dependent: :destroy
   has_many :tastes, through: :food_tastes
-
-  # この料理が指定された味を持っているかどうか
-  #
-  # @param [Fixnum] taste_id 味ID
-  # @return [TrueClass, FalseClass] 持っているとtrue
-  def have_taste?(taste_id)
-    self.taste_ids.include?(taste_id)
-  end
 end
