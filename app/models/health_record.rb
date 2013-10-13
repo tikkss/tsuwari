fd7909a39f4ab6ae71b40d57d2954378365a2c4c
tsutomu katsube <tsuto.igawa@gmail.com>
2013-10-14 00:30:12 +0900

    modifying HealthRecord validations, input ways and displaying

--- app/models/health_record.rb
+++ app/models/health_record.rb
@@ -1,2 +1,31 @@
 class HealthRecord < ActiveRecord::Base
+  TIME_PERIODS = [
+    {value: 1, label: '朝'},
+    {value: 2, label: '昼'},
+    {value: 3, label: '晩'}
+  ]
+  HEALTHS = [
+    {value: 1, label: '悪い'},
+    {value: 2, label: '普通'},
+    {value: 3, label: '良い'}
+  ]
+
+  def get_time_period
+    ret = {}
+    TIME_PERIODS.each do |t|
+      ret = t if t[:value] == time_period
+    end
+    return ret
+  end
+
+  def get_health
+    ret = {}
+    HEALTHS.each do |h|
+      ret = h if h[:value] == health
+    end
+    return ret
+  end
+
+  validates :time_period, inclusion: { in: TIME_PERIODS.map{ |time_period| time_period[:value] } }
+  validates :health,      inclusion: { in: HEALTHS.map{ |health| health[:value] } }
 end
--- spec/views/health_records/index.html.erb_spec.rb
+++ spec/views/health_records/index.html.erb_spec.rb
@@ -17,7 +17,7 @@ describe "health_records/index" do
   it "renders a list of health_records" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => '朝', :count => 2
+    assert_select "tr>td", :text => '普通'.to_s, :count => 2
   end
 end
