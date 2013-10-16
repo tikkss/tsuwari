2cd8d3554b9ce375d4c2f570daf9d04997797c53
pbz-matsukawa <k-matsukawa@probizmo.co.jp>
2013-10-16 18:08:48 +0900

    modify shop_spec and category_spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,9 @@
 require 'spec_helper'
 
 describe Category do
+  it { should have_many(:shops) }
+  it { should validate_presence_of(:name) }
+  
   describe "associations" do
     before do
       @category = Category.create(name: "カテゴリ")
@@ -9,33 +12,6 @@ describe Category do
     end
     it "カテゴリ削除により、関連する店舗のカテゴリIDをnilにすること" do
       expect { @category.destroy }.to change { Shop.first.category_id }.to(nil)
-    end
-  end
-  
-  describe "#valid?" do
-    before { obj.valid? }
-    describe "name" do
-      let(:column) { :name }
-      context "空白の場合" do
-        let(:obj) { Category.new(name: "") }
-        it "エラーが発生すること" do
-          obj.should have(1).error_on(column)
-        end
-      end
-      context "設定する場合" do
-        context "全角文字の場合" do
-          let(:obj) { Category.new(name: "カテゴリ") }
-          it "エラーが発生しないこと" do
-            obj.should have(0).error_on(column)
-          end
-        end
-        context "半角文字の場合" do
-          let(:obj) { Category.new(name: "category") }
-          it "エラーが発生しないこと" do
-            obj.should have(0).error_on(column)
-          end
-        end
-      end
     end
   end
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,52 +1,29 @@
 require 'spec_helper'
 
 describe Shop do
-  describe "#valid?" do
-    before { obj.valid? }
-    context "全属性が空の場合" do
-      let(:obj) { Shop.new }
-      it "エラーが発生しないこと" do
-        obj.should have(0).error_on(:name)
-        obj.should have(0).error_on(:prefecture)
-        obj.should have(0).error_on(:city)
-        obj.should have(0).error_on(:address)
-        obj.should have(0).error_on(:url)
-        obj.should have(0).error_on(:category_id)
-      end
-    end
-    
-    shared_examples_for "バリデーションエラーが発生しないこと" do |model, column, input|
-      let(:obj) { model.new(column => input) }
-      it { obj.should have(0).error_on(column) }
-    end
-    describe "name" do
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :name, 1
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :name, "abc"
-    end
-    describe "prefecture" do
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :prefecture, 1
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :prefecture, "abc"
-    end
-    describe "city" do
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :city, 1
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :city, "abc"
-    end
-    describe "address" do
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :address, 1
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :address, "abc"
-    end
-    describe "url" do
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :url, 1
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :url, "abc"
-    end
-    describe "category_id" do
-      it_behaves_like "バリデーションエラーが発生しないこと", Shop, :category_id, 1
-      context "対象カラムに文字列を入れた場合" do
-        let(:obj) { Shop.new(:category_id => "abc") }
-        it "エラーが発生すること" do
-          obj.should have(1).error_on(:category_id)
-        end
-      end
-    end
-  end
+  it { should belong_to(:category) }
+
+  it { should allow_value(nil).for(:name) }
+  it { should allow_value(1).for(:name) }
+  it { should allow_value("abc").for(:name) }
+
+  it { should allow_value(nil).for(:prefecture) }
+  it { should allow_value(1).for(:prefecture) }
+  it { should allow_value("abc").for(:prefecture) }
+
+  it { should allow_value(nil).for(:city) }
+  it { should allow_value(1).for(:city) }
+  it { should allow_value("abc").for(:city) }
+
+  it { should allow_value(nil).for(:address) }
+  it { should allow_value(1).for(:address) }
+  it { should allow_value("abc").for(:address) }
+
+  it { should allow_value(nil).for(:url) }
+  it { should allow_value(1).for(:url) }
+  it { should allow_value("abc").for(:url) }
+
+  it { should allow_value(nil).for(:category_id) }
+  it { should allow_value(1).for(:category_id) }
+  it { should_not allow_value("abc").for(:category_id) }
 end
