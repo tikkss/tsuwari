6b52f1a73da4cfe5fb7ef3eb3ca655c9cb825cf2
sousk333 <fire.bluestar@gmail.com>
2013-10-16 22:00:37 +0900

    Modified. food spec has_many validate

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Food do
   describe "validate" do
+    it { should have_many(:food_tastes).dependent(:destroy) }
+    it { should have_many(:tastes).through(:food_tastes) }
     it { should validate_presence_of(:name) }
     it { should ensure_length_of(:name).is_at_most(255) }
   end
