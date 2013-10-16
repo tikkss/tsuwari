84f7feadfe22a5dcd06267b22f08c3fe9eb8390c
sousk333 <fire.bluestar@gmail.com>
2013-10-16 22:04:58 +0900

    Added. Food.search_foods 料理検索メソッド

--- app/models/food.rb
+++ app/models/food.rb
@@ -9,5 +9,18 @@ class Food < ActiveRecord::Base
   # @param [Hash] food_params 検索フォームから
   # @return [ActiveRecord::Relation] Food検索結果
   def self.search_foods(food_params)
+    arel = includes(:tastes).order(:name)
+    unless food_params[:name].blank?
+      arel = arel.where(
+        "foods.name like :name", name: "%#{food_params[:name]}%"
+      )
+    end
+
+    food_params[:taste_ids].delete_if { |v| v.blank? }
+    unless food_params[:taste_ids].blank?
+      arel = arel.where(tastes: { id: food_params[:taste_ids] })
+    end
+
+    arel
   end
 end
