54d8a5166c5d21bcf5e9763ba8b56a568c150a88
sousk333 <fire.bluestar@gmail.com>
2013-10-16 22:02:42 +0900

    Fixed. foods index view

--- spec/views/foods/index.html.erb_spec.rb
+++ spec/views/foods/index.html.erb_spec.rb
@@ -1,36 +1,20 @@
 require 'spec_helper'
 
 describe "foods/index" do
-  before(:each) do
-    @expect_tastes1 = %w(甘い あっさり)
-    @expect_tastes2 = %w(辛い 苦い こってり)
-
-    assign(:search_food, stub_model(Food))
-
-    assign(
-      :tastes,
-      (@expect_tastes1 + @expect_tastes2).map do |name|
-        stub_model(Taste, name: name)
-      end
-    )
-
-    assign(:foods, [
-      stub_model(Food,
-        name: "Name",
-        tastes: @expect_tastes1.map { |name| stub_model(Taste, name: name) }
-      ),
-      stub_model(Food,
-        name: "Name",
-        tastes: @expect_tastes2.map { |name| stub_model(Taste, name: name) }
-      )
-    ])
+  before do
+    @food1 = create(:food, tastes: create_list(:taste, 3))
+    @food2 = create(:food, tastes: create_list(:taste, 3))
+    assign(:search_food, build(:food))
+    assign(:tastes, Taste.all)
+    assign(:foods, [@food1, @food2])
   end
 
   it "renders a list of foods" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", text: "Name".to_s, count: 2
-    assert_select "tr>td", text: @expect_tastes1.join(", "), count: 1
-    assert_select "tr>td", text: @expect_tastes2.join(", "), count: 1
+    assert_select "tr>td", text: @food1.name, count: 1
+    assert_select "tr>td", text: @food2.name, count: 1
+    assert_select "tr>td", text: @food1.tastes.pluck(:name).join(", "), count: 1
+    assert_select "tr>td", text: @food2.tastes.pluck(:name).join(", "), count: 1
   end
 end
